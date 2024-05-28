0173d9c7b6403cda87eb0644d4db497730119348
Serge Buasa <sergebuasa@gmail.com>
2024-05-28 15:09:57 +0200

    requete 1

--- queries.py
+++ queries.py
@@ -1,8 +1,15 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from create_table import Base, Book
+from create_table import Base, Book, engine
+
+
+Session = sessionmaker(bind=engine)
+session_queries = Session()
 
 # nombre de livres importés
+nombre_livre = session_queries.query(Book).count()
+print(f"nombre de livres importés: {nombre_livre}")
+
 
 
 
